e5bbd0bc7864e6813b7b10ef020f5d7122930124
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-04-10 12:26:11 +0500

    Added monsters

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +13,27 @@ namespace DeathPitTest
 {
     public partial class Form1 : Form
     {
+        int monsterUnitSpeed = 3;
+        int targetCount = 25;
+        Random randNum = new Random();
+        List<PictureBox> monsterUnitList = new List<PictureBox>();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void MakeZombies()
+        {
+            PictureBox monster = new PictureBox();
+            monster.Tag = "monster";
+            //monster.Image = Properties.Resources.down;
+            monster.Left = randNum.Next(0, 900);
+            monster.Top = randNum.Next(0, 800);
+            monster.SizeMode = PictureBoxSizeMode.AutoSize;
+            monsterUnitList.Add(monster);
+            this.Controls.Add(monster);
+           // Player.BringToFront();
+
+        }
     }
 }
