c8c938512edd7bb45bebfb187c2f10fa182a309f
Dmitriz1 <stud0000264980@study.utmn.ru>
2023-05-11 15:56:39 +0500

    метод восстановления патронов работает, нужно добавить картинку, можно сделать метод восстановления HP аналогичным методом

--- Form2.cs
+++ Form2.cs
@@ -97,7 +97,7 @@ namespace DeathPitTest
                 Player.Top += HeroSpeed;
             }
 
-            if (goLeft && Player.Left > 0)
+            if (goLeft && Player.Left > 0) 
             {
                 Health(playerHP);
                 Player.Left -= HeroSpeed;
@@ -111,16 +111,15 @@ namespace DeathPitTest
 
             foreach (Control x in this.Controls)////////////////////////////////////////////////////
             {
-                //if (x is PictureBox && (string)x.Tag == "ammo")
-                //{
-                //    if (player.Bounds.IntersectsWith(x.Bounds))
-                //    {
-                //        this.Controls.Remove(x);
-                //        ((PictureBox)x).Dispose();
-                //        ammo += 5;
-
-                //    }
-                //}
+                if (x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (Player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        HeroAmmo += 5;
+                    }
+                }
 
 
                 if (x is PictureBox && (string)x.Tag == "monster")
@@ -355,7 +354,7 @@ namespace DeathPitTest
         private void DropAmmo()
         {
             PictureBox ammo = new PictureBox();
-            ammo.Image = Properties.Resources.ammo;
+            ammo.Image = Properties.Resources.halfHurt;
             ammo.SizeMode = PictureBoxSizeMode.AutoSize;
             ammo.Left = randNum.Next(10, this.ClientSize.Width - ammo.Width);
             ammo.Top = randNum.Next(10, this.ClientSize.Height - ammo.Height);
---
+++ ShellShot.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DeathPitTest
+{
+    internal class ShellShot
+    {
+        public string direction;
+        public int ballLeft;
+        public int ballTop;
+
+        private int speed = 20;
+        private PictureBox ball = new PictureBox();
+        private Timer ballTimer = new Timer();
+
+
+        public void MakeSGball(Form form)
+        {
+            ball.BackColor = Color.Yellow;
+            ball.Size = new Size(5, 5);
+            ball.Tag = "ball";
+            ball.Left = ballLeft;
+            ball.Top = ballTop;
+            ball.BringToFront();
+
+            form.Controls.Add(ball);
+
+            ballTimer.Interval = speed;
+            ballTimer.Tick += new EventHandler(BallTimerEvent);
+            ballTimer.Start();
+        }
+
+        private void BallTimerEvent(object sender, EventArgs e)
+        {
+            if (direction == "left")
+            {
+                ball.Left -= speed;
+            }
+            if (direction == "right")
+            {
+                ball.Left += speed;
+            }
+            if (direction == "up")
+            {
+                ball.Top -= speed;
+            }
+            if (direction == "down")
+            {
+                ball.Top += speed;
+            }
+
+            if (ball.Left < 0 || ball.Left > 1920 || ball.Top < 0 || ball.Top > 1080)
+            {
+                ballTimer.Stop();
+                ballTimer.Dispose();
+                ball.Dispose();
+                ballTimer = null;
+                ball = null;
+            }
+        }
+
+    }
+}
