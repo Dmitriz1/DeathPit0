b2d5ab2d93ed113a50736de3cf78c4a11be208a2
gospod1spas1 <outofbrain72@gmail.com>
2023-05-17 23:11:38 +0500

    small options form fix

--- OptionsForm.cs
+++ OptionsForm.cs
@@ -16,11 +16,12 @@ namespace DeathPitTest
         {
             InitializeComponent();
             richTextBox1.Text = "Настроек нет, зато есть обучение:\n\n\n\n" +
-                "Стрелять на \"Space"\.\n\n" +
+                "Стрелять на \"Space\"\\n\n" +
                 "Мало патронов? Нажми \"R\". Если патронов больше 1, нажать не получится.\n\n" +
                 "Мало хп? Нажми \"Н\". Если больше половины сердечка, ты еще живее всех живых.\n\n" +
                 "Пауза на \"Escape\". Хотя, возможно, эта подсказка тебе и не понадобится.\n\n\n\n" +
                 "Удачи!";
+            richTextBox1.Enabled = false;
         }
 
         private void BackButton_Click(object sender, EventArgs e)
